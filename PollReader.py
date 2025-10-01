07710a66f1be48dd48b016f2716ec573ca857c1b
Abby Collins <abbyco@umich.edu>
2025-10-01 13:49:01 -0400

    committing

--- PollReader.py
+++ PollReader.py
@@ -121,6 +121,13 @@ class PollReader():
 
 
     def polling_history_change(self):
+        early_harris = sum(self.data_dict['Harris result'][-30:]) / 30
+        early_trump = sum(self.data_dict['Trump result'][-30:]) / 30
+
+        late_harris = sum(self.data_dict['Harris result'][:30]) / 30
+        late_trump = sum(self.data_dict['Trump result'][:30]) / 30
+
+        return((late_harris - early_harris), (late_trump - early_trump))
         """
         Calculate the change in polling averages between the earliest and latest polls.
 
